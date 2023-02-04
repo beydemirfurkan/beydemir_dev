a5b6c43e7ae44abb4dd1ad5a100e39f75967f185
Furkan Beydemir <70379120+beydemirfurkan@users.noreply.github.com>
2023-02-04 13:50:51 +0300

    first commit

---
+++ src/pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <h1>Test Beydemir</h1>
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
